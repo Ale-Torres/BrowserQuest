67cce42879d34aa668efb90807b74971d68f49e1
Ale-Torres <46111936+Ale-Torres@users.noreply.github.com>
2022-03-18 15:41:53 -0700

    Create processmap.test.js

---
+++ client/js/lib/tests/processmap.test.js
@@ -0,0 +1,57 @@
+/*JS Unit testing using Jest*/
+const processmap = require('./processmap').processmap;
+
+/*test function input output */
+describe('processMap', () => {
+   test('client_name', () => {
+      //inputs
+      const Info = '';
+      const description = '';
+      const jestinformation = '';
+
+      //test
+      const result = extractInfo(Info);
+    
+      //expect
+      expect(result).toEqual(expected);
+   });
+  
+     test('length', () => {
+      //inputs
+      const Info = '';
+      const description = '';
+      const jestinformation = '';
+
+      //test
+      const result = extractInfo(Info);
+    
+      //expect
+      expect(result).toEqual(expected);
+   });
+  
+     test('delay', () => {
+      //inputs
+      const Info = '';
+      const description = '';
+      const jestinformation = '';
+
+      //test
+      const result = extractInfo(Info);
+    
+      //expect
+      expect(result).toEqual(expected);
+   });
+  
+     test('client_name', () => {
+      //inputs
+      const Info = '';
+      const description = '';
+      const jestinformation = '';
+
+      //test
+      const result = extractInfo(Info);
+    
+      //expect
+      expect(result).toEqual(expected);
+   });
+});
